63ea11132068329b3c47ea6849bf4064737e6b8f
Sabine Mejia <smejia@goldengate.cs.brynmawr.edu>
2023-02-23 22:15:19 -0500

    Still in progress

--- A05/read_ppm.c
+++ A05/read_ppm.c
@@ -1,12 +1,15 @@
 /*----------------------------------------------
- * Author: 
- * Date: 
- * Description
+ * Author: Sabine Mejia 
+ * Date: 02/23/2023
+ * Description: This program contains a method that can read PPM files stored 
+ * in binary format. This function takes a filename as input and return 
+ * a 2D array of struct pixel. 
  ---------------------------------------------*/
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include "read_ppm.h"
+
 
 // Choose *one* to implement (do not remove the other one!)
 
@@ -14,6 +17,22 @@ struct ppm_pixel* read_ppm(const char* filename, int* w, int* h) {
   return NULL;
 }
 
+
 struct ppm_pixel** read_ppm_2d(const char* filename, int* w, int* h) {
+  
+  FILE* fp;
+  char str[1028];
+
+  /* opening file for reading */
+  fp = fopen(filename, "r");
+   if(fp == NULL) {
+    perror("Error opening file");
+   }
+   if(fgets(str, 60, fp)!= NULL) {
+      /* writing content to stdout */
+      puts(str);
+   }
+   fclose(fp);
+  
   return NULL;
 }
