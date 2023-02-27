a049618bff4805ef562c74cc16f377798d383cbe
Sabine Mejia <smejia@goldengate.cs.brynmawr.edu>
2023-02-27 15:24:49 -0500

    02/27

--- A05/read_ppm.c
+++ A05/read_ppm.c
@@ -22,17 +22,92 @@ struct ppm_pixel** read_ppm_2d(const char* filename, int* w, int* h) {
   
   FILE* fp;
   char str[1028];
+  char* token;
+
+  
 
   /* opening file for reading */
-  fp = fopen(filename, "r");
+  fp = fopen(filename, "rb");
    if(fp == NULL) {
     perror("Error opening file");
    }
-   if(fgets(str, 60, fp)!= NULL) {
-      /* writing content to stdout */
-      puts(str);
+   //reads 1st line of header
+   if(fgets(str, 1028, fp)!= NULL) {
    }
+   //read 2nd line of header
+   if(fgets(str, 1028, fp)!= NULL) {
+   }
+   //read 3rd line of header (this contains the dimenstions)
+   if(fgets(str, 1028, fp) != NULL) {
+      token = strtok(str, " ");
+      *w = atoi(token);
+
+      token = strtok(NULL, " ");
+      *h = atoi(token);
+      
+      printf("dimensions: %d %d \n", *w, *h);
+   }
+   //read 4th line of header
+   if(fgets(str, 1028, fp)!= NULL) {
+   }
+
+   //read rest of file
+   if(fgets(str, 1028, fp) != NULL) {
+
+      printf("hi %s", str);
+      token = strtok(str, " "); //get first token
+      puts(str);
+      puts(token);
+
+      //declare and allocate space for a 2D array of type struct pointer
+      struct ppm_pixel** pixel_array;
+      pixel_array = malloc(sizeof(struct ppm_pixel*) * *h);
+      if (pixel_array == NULL) {
+         printf("Error: malloc failed\n");
+         exit(1);
+      }
+      for(int i = 0; i < *h; i++) {
+         pixel_array[i] = malloc(sizeof(struct ppm_pixel) * *w);
+         if (pixel_array[i] == NULL) {
+         printf("Error: malloc failed\n");
+         exit(1);
+         }
+      }
+      printf("%u \n", pixel_array[3][3].red);
+
+
+      for(int row = 0; row < *h; row++) {
+
+         for(int col = 0; col < *w; col++) {
+
+            struct ppm_pixel new_pixel;
+
+            fread(&new_pixel, sizeof(struct ppm_pixel), 1, fp);
+
+            pixel_array[row][col] = new_pixel;
+            /**
+            pixel_array[row][col].red = *token;
+            token = strtok(NULL, " ");
+            pixel_array[row][col].green = *token;
+            token = strtok(NULL, " ");
+            pixel_array[row][col].blue = *token;
+            token = strtok(NULL, " ");
+            **/
+         }
+      }
+
+   } 
+  
+   
+
+   
+
    fclose(fp);
   
   return NULL;
+
+
+  //malloc a 2D array of ppm_pixel structs
+  
+
 }
