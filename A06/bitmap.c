3772b5ef68a4a06883bb17b81d3bcc5fee9b09d1
Sabine Mejia <smejia@goldengate.cs.brynmawr.edu>
2023-03-03 09:43:13 -0500

    bitmap.c finished yesterday, still working on decode.c and and will start encode.c once I finish decode.c, sorry for the delay!

--- A06/bitmap.c
+++ A06/bitmap.c
@@ -1,15 +1,39 @@
 /*----------------------------------------------
- * Author: 
- * Date: 
- * Description
+ * Author: Sabine Mejia 
+ * Date: 03-03-23
+ * Description: This program reads in a unsigned floating point number then
+ * converts it to binary and prints an 8 x 8 1bpp image. A 0 in binary 
+ * corresponds to a space and a 1 corresponds to an @.
  ---------------------------------------------*/
 #include <stdio.h>
 
 int main() {
   unsigned long img;
+  unsigned long mask;
+  unsigned long masked_img;
+  int count = 1;
+  
   scanf(" %lx", &img);
   printf("Image (unsigned long): %lx\n", img);
 
   // todo: your code here
+  
+
+  for(int i = 63; i >= 0; i--) {
+    mask = 0x1ul << i;
+    masked_img = mask & img;
+    
+    if(masked_img != 0) {
+      printf("@");
+    }
+    if(masked_img == 0) {
+      printf(" ");
+    }
+    if((count % 8 == 0)) {
+      printf("\n");
+    }
+    count ++;
+  }
+  printf("-----------------------\n");
   return 0;
 }
--- A06/decode.c
+++ A06/decode.c
@@ -1,17 +1,103 @@
 /*----------------------------------------------
- * Author: 
- * Date: 
- * Description
+ * Author: Sabine Mejia 
+ * Date: 03-03-23
+ * Description: This program reads in a .ppm file and extracts a secrete message
+ * from it by recording the last digit of every color stream in a pixel and
+ * then for every 8 binary numbers finding the corresponding ASCII character.
  ---------------------------------------------*/
 #include <stdio.h>
 #include <stdlib.h>
 #include "read_ppm.h"
+#include <string.h>
+
+//takes in a 2d array and returns a 1d array
+struct ppm_pixel* to_one_d_array(struct ppm_pixel** two_d_pixel_arr, int w, int h) {
+  struct ppm_pixel* one_d_arr = malloc(sizeof(struct ppm_pixel) * w * h);
+
+  int c = 0;
+  for(int i = 0; i < h; i++) {
+    for(int j = 0; j < w; j++) {
+      one_d_arr[c] = two_d_pixel_arr[i][j];
+      c++;
+    }
+  }
+  return one_d_arr;
+}
+
+int fromBinary(char *s) {
+  return (int) strtol(s, NULL, 2);
+}
 
 int main(int argc, char** argv) {
+  int w, h;
+  struct ppm_pixel** two_d_pixel_arr;
+  struct ppm_pixel* one_d_pixel_arr; 
+  const char* filename;
+  char* bin_str;
+  char* eight_bit_str;
+  int integer;
+
   if (argc != 2) {
     printf("usage: decode <file.ppm>\n");
     return 0;
   }
+
+  filename = argv[1];
+  two_d_pixel_arr = read_ppm_2d(filename, &w, &h);
+  one_d_pixel_arr = to_one_d_array(two_d_pixel_arr, w, h);
+
+  printf("Reading %s with width: %d and height: %d\n", filename, w, h);
+
+
+  bin_str = malloc(sizeof(int) * 3 * w * h);
+  int c = 0;
+  for(int i = 0; i < w * h; i++) {
+    if(one_d_pixel_arr[i].red % 2 == 0) {
+      bin_str[c] = 0;
+    }
+    else {
+      bin_str[c] = 1;
+    }
+    c++;
+    if(one_d_pixel_arr[i].green % 2 == 0) {
+      bin_str[c] = 0;
+    }
+    else {
+      bin_str[c] = 1;
+    }
+    c++;
+    if(one_d_pixel_arr[i].blue % 2 == 0) {
+      bin_str[c] = 0;
+    }
+    else {
+      bin_str[c] = 1;
+    }
+    c++;
+  }
+
+  for(int i = 0; i < 3 * w * h; i++) {
+    printf("%d", bin_str[i]);
+  }
+  printf("\n");
+
+  eight_bit_str = malloc(sizeof(char) * 8);
+  c = 0;
+  for(int i = 0; i < 3 * w * h; i++) {
+    if(c < 8) {
+      eight_bit_str[c] = bin_str[i];
+      printf("%d\n", bin_str[i]);
+    }
+    c++;
+    if(c == 8) {
+      integer = fromBinary(eight_bit_str);
+      printf("%c\n", (char)(integer));
+      c = 0;
+    }
+    
+  }
+  
+
+
 
   return 0;
 }
