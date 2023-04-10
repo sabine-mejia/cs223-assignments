debc34e3add3908632b8e30c19a36873c8fda82e
Sabine Mejia <smejia@goldengate.cs.brynmawr.edu>
2023-04-10 15:50:41 -0400

    pushing read and write ppm files

--- A06/read_ppm.c
+++ A06/read_ppm.c
@@ -1,12 +1,16 @@
 /*----------------------------------------------
- * Author: 
- * Date: 
- * Description
+ * Author: Sabine Mejia 
+ * Date: 02/23/2023
+ * Description: This program contains a method that can read PPM files stored 
+ * in binary format. This function takes a filename and pointers to the variables
+ * which store the height and width as input and returns a 2D array of type 
+ * struct pixel. 
  ---------------------------------------------*/
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include "read_ppm.h"
+
 
 // Choose *one* to implement (do not remove the other one!)
 
@@ -14,6 +18,56 @@ struct ppm_pixel* read_ppm(const char* filename, int* w, int* h) {
   return NULL;
 }
 
+
 struct ppm_pixel** read_ppm_2d(const char* filename, int* w, int* h) {
-  return NULL;
+  
+  FILE* fp;
+  char str[1028];
+  struct ppm_pixel** pixel_array;
+
+  
+
+  /* opening file for reading */
+  fp = fopen(filename, "rb");
+   if(fp == NULL) {
+    perror("Error opening file");
+   }
+   //reads 1st line of header
+   fgets(str, 1028, fp);
+   //read 2nd line of header
+   fgets(str, 1028, fp);
+   //read 3rd line of header (this contains the dimenstions)
+   fgets(str, 1028, fp);
+   sscanf(str, "%d %d", w, h);
+   //read 4th line of header
+   fgets(str, 1028, fp);
+
+      //declare and allocate space for a 2D array of type struct pointer
+      
+      pixel_array = malloc(sizeof(struct ppm_pixel*) * *h);
+      if (pixel_array == NULL) {
+         printf("Error: malloc failed\n");
+         exit(1);
+      }
+      for(int i = 0; i < *h; i++) {
+         pixel_array[i] = malloc(sizeof(struct ppm_pixel) * *w);
+         if (pixel_array[i] == NULL) {
+         printf("Error: malloc failed\n");
+         exit(1);
+         }
+      }
+
+   //read rest of file and store it in struct
+   for(int i = 0; i < *h; i ++) {
+      for(int j = 0; j < *w; j++) {
+         fread(&(pixel_array[i][j]), sizeof(struct ppm_pixel), 1, fp);
+      }
+   }
+      
+
+   fclose(fp);
+  
+  return pixel_array;
+  
+
 }
--- A06/write_ppm.c
+++ A06/write_ppm.c
@@ -1,7 +1,10 @@
 /*----------------------------------------------
- * Author: 
- * Date: 
- * Description
+ * Author: Sabine Mejia
+ * Date: 03/05/23
+ * Description: This program implements the method write_ppm_2d which takes
+ * a filename, 2D pixel array, and width and height as parameters. write_ppm_2d
+ * then opens the file and writes the information contained in the 2D pixel array
+ * into the file in hexidecimal.
  ---------------------------------------------*/
 #include "write_ppm.h"
 #include <stdio.h>
@@ -15,4 +18,15 @@ void write_ppm(const char* filename, struct ppm_pixel* pixels, int w, int h) {
 
 void write_ppm_2d(const char* filename, struct ppm_pixel** pixels, int w, int h) {
 
+    FILE * fp;
+    fp = fopen(filename, "w+");
+    fprintf(fp, "P6\n#Comment\n%d %d\n255\n", w, h);
+   
+    for(int i = 0; i < h; i ++) {
+        for(int j = 0; j < w; j++) {
+            fwrite(&pixels[i][j], 1, 3, fp); //or replace 3 with 3* *w * *h???
+        }
+    }
+    
+    fclose(fp);
 }
