cf0a10d3de6fffbf05bfd8b299e88d1ab5313bf3
Sabine Mejia <smejia@goldengate.cs.brynmawr.edu>
2023-04-10 15:53:14 -0400

    adding read and write ppm files

--- A09/write_ppm.c
+++ A09/write_ppm.c
@@ -6,4 +6,15 @@ void write_ppm(const char* filename, struct ppm_pixel* pixels, int w, int h) {
 }
 
 void write_ppm_2d(const char* filename, struct ppm_pixel** pxs, int w, int h) {
+    FILE * fp;
+    fp = fopen(filename, "w+");
+    fprintf(fp, "P6\n#Comment\n%d %d\n255\n", w, h);
+   
+    for(int i = 0; i < h; i ++) {
+        for(int j = 0; j < w; j++) {
+            fwrite(&pxs[i][j], 1, 3, fp); //or replace 3 with 3* *w * *h???
+        }
+    }
+    
+    fclose(fp);
 }
